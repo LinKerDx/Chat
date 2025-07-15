e4345062859017cfc06b946252a8f1db4045bbf2
LinKDx <omar_150596@hotmail.com>
2025-07-14 21:35:06 -0600

    progresing

--- server/index.js
+++ server/index.js
@@ -1,15 +1,33 @@
 import express from 'express'
 import logger from 'morgan'
 
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
+
 const port = process.env.PORT ?? 3000
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('conection', (socket) => {
+    console.log('A user connected')
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected')
+    })
+
+    socket.on('chat message', (msg) => {
+        console.log('Message: ' + msg)
+    })
+})
+
 app.use(logger('dev'))
 
 app.get('/', (req, res) => {
     res.sendFile(process.cwd() + '/client/index.html')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
