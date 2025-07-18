fccfb5a882a4b15dbe10e88425c057a5bf898c40
LinKDx <omar_150596@hotmail.com>
2025-07-17 18:56:06 -0600

    Adding data persistence

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 import express from 'express'
 import logger from 'morgan'
+import dotenv from 'dotenv'
+import { createClient } from '@libsql/client'
 
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
+
+
+dotenv.config()
 
 const port = process.env.PORT ?? 3000
 
@@ -14,16 +19,52 @@ const io = new Server(server, {
     }
 })
 
-io.on('connection', (socket) => {
+const db = createClient({
+    url: 'libsql://select-maginty-linkerdx.aws-us-east-2.turso.io',
+    authToken: process.env.DB_TOKEN
+})
+
+await db.execute(`CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT NOT NULL,
+    user TEXT
+)`)
+
+io.on('connection', async (socket) => {
     console.log('A user connected')
 
     socket.on('disconnect', () => {
         console.log('A user disconnected')
     })
 
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg)
+    socket.on('chat message', async (msg) => {
+        let result
+        const user = socket.handshake.auth.user ?? 'Anonymous'
+        try {
+            result = await db.execute({
+                sql: 'INSERT INTO messages (content, user) VALUES (:msg, :user)',
+                args: { msg, user }
+            })
+        } catch (error) {
+            console.error('Error inserting message:', error)
+            return
+        }
+        io.emit('chat message', msg, result.lastInsertRowid.toString(), user)
     })
+
+    if (!socket.recovered) {
+        try {
+            const results = await db.execute({
+                sql: 'SELECT id, content, user FROM messages WHERE id > ?',
+                args: [socket.handshake.auth.serverOffset ?? 0],
+            })
+            results.rows.forEach(row => {
+                socket.emit('chat message', row.content, row.id.toString(), row.user)
+            })
+        } catch (e) {
+            console.error('Error fetching messages:', e)
+        }
+    }
 })
 
 app.use(logger('dev'))
